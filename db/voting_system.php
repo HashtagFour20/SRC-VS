d9a0dcb59dd80764a2cecf276623d3e67936f650
Keenan Barends <71004622+HashtagFour20@users.noreply.github.com>
2021-05-26 02:06:50 +0200

    Add files via upload

---
+++ db/voting_system.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `voting_system`
+ */
+
+/* `voting_system`.`admin` */
+$admin = array(
+  array('username' => 'admin1','password' => 'password1234'),
+  array('username' => 'admin2','password' => 'password1234')
+);
+
+/* `voting_system`.`candidates` */
+$candidates = array(
+  array('candidate_id' => '1','candidate_name' => 'Dalia','candidate_surname' => 'Benton','political_party' => 'EFFSC','faculty' => 'Education'),
+  array('candidate_id' => '2','candidate_name' => 'Barney','candidate_surname' => 'Herbert','political_party' => 'PASMA','faculty' => 'Applied Sciences'),
+  array('candidate_id' => '3','candidate_name' => 'Wendy','candidate_surname' => 'Gadeni','political_party' => 'PASMA','faculty' => 'Business and Managments Sciences'),
+  array('candidate_id' => '4','candidate_name' => 'Xolani','candidate_surname' => 'Makosa','political_party' => 'PASMA','faculty' => 'Education'),
+  array('candidate_id' => '5','candidate_name' => 'Samantha','candidate_surname' => 'Isaac','political_party' => 'SASCO','faculty' => 'Engineering & Built Environment'),
+  array('candidate_id' => '6','candidate_name' => 'Pumi','candidate_surname' => 'Msutu','political_party' => 'SASCO','faculty' => 'Education'),
+  array('candidate_id' => '7','candidate_name' => 'Rick','candidate_surname' => 'Van De Vent','political_party' => 'EFFSC','faculty' => 'Business & Management Sciences'),
+  array('candidate_id' => '8','candidate_name' => 'Siyathanda','candidate_surname' => 'Moyo','political_party' => 'SASCO','faculty' => 'Education'),
+  array('candidate_id' => '9','candidate_name' => 'Sinazo','candidate_surname' => 'Madikazi','political_party' => 'PASMA','faculty' => 'Health & Wellness Sciences'),
+  array('candidate_id' => '10','candidate_name' => 'Zimkitha','candidate_surname' => 'Guza','political_party' => 'PASMA','faculty' => 'Engineering & Built Environment'),
+  array('candidate_id' => '11','candidate_name' => 'Lukhanyo','candidate_surname' => 'Saba','political_party' => 'Independent','faculty' => 'Applied Sciences'),
+  array('candidate_id' => '12','candidate_name' => 'Karabo','candidate_surname' => 'Kunene','political_party' => 'EFFSC','faculty' => 'Education'),
+  array('candidate_id' => '13','candidate_name' => 'Noyolo','candidate_surname' => 'Hashe','political_party' => 'SASCO','faculty' => 'Applied Sciences'),
+  array('candidate_id' => '14','candidate_name' => 'Siphokazi','candidate_surname' => 'Banti','political_party' => 'PASMA','faculty' => 'Education'),
+  array('candidate_id' => '15','candidate_name' => 'Lungile','candidate_surname' => 'Fati','political_party' => 'Independent','faculty' => 'Business & Management Sciences'),
+  array('candidate_id' => '16','candidate_name' => 'Nasiphi','candidate_surname' => 'Tshefu','political_party' => 'EFFSC','faculty' => 'Business & Management Sciences')
+);
+
+/* `voting_system`.`position` */
+$position = array(
+  array('postition_id' => '1','position_title' => 'Chairperson'),
+  array('postition_id' => '2','position_title' => 'Deputy-Chairperson'),
+  array('postition_id' => '3','position_title' => 'Secretary'),
+  array('postition_id' => '4','position_title' => 'Treasurer'),
+  array('postition_id' => '5','position_title' => 'LHC & Wellfare Officer'),
+  array('postition_id' => '6','position_title' => 'Sports, Arts & Culture Officer'),
+  array('postition_id' => '7','position_title' => 'Communications Officer'),
+  array('postition_id' => '8','position_title' => 'Projects & Development Officer'),
+  array('postition_id' => '9','position_title' => 'Local International Relations Officer')
+);
+
+/* `voting_system`.`student` */
+$student = array(
+  array('student_id' => '219002959','student_name' => 'Keenan','student_surname' => 'Barends','student_email' => '219002959@mycput.ac.za','faculty' => 'ICT')
+);
+
+/* `voting_system`.`vote` */
+$vote = array(
+);
