cbfa8ec4f92b6efd44a9556bb5cb1e0094a01aa1
michael-onsight <michael@onsightapp.com>
2021-09-16 01:09:16 +0200

    Made some improvements to the admin portal

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -69,19 +69,36 @@
             <ul class="nav nav-pills nav-stacked flex-column">
               <li class="nav-header">Dashboards</li>
               <li class="nav-item">
-                <a class="nav-link active" href="index.php">Overview</a>
+                <a class="nav-link active" href="index.html">Overview</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link " href="currentmembers/index.php">Current Members</a>
+                <a class="nav-link " href="order-history/index.html">Order history</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link " href="votingperiod/index.php">Voting period</a>
+                <a class="nav-link "href="fluid/index.html">Fluid layout</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link " href="candidates/index.php">Candidates</a>
+                <a class="nav-link " href="icon-nav/index.html">Icon nav</a>
+              </li>
+
+              <li class="nav-header">More</li>
+              <li class="nav-item">
+                <a class="nav-link "href="docs/index.html">
+                  Toolkit docs
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link " href="registrations/index.php">Registrations</a>
+                <a class="nav-link" href="http://getbootstrap.com" target="blank">
+                  Bootstrap docs
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="index-light/index.html">Light UI</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#docsModal" data-toggle="modal">
+                  Example modal
+                </a>
               </li>
             </ul>
             <hr class="visible-xs mt-3">
