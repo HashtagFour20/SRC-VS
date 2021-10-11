e688cc43745de89d59e86405d5ee0c1c2dd66266
Faidh <faidhadams@gmail.com>
2021-10-11 19:54:03 +0200

    add result variable for ballot

--- voting/ballot.php
+++ voting/ballot.php
@@ -13,14 +13,25 @@
        die("Connection failed: " . $conn->connect_error);
      }
 
+    //fetches student number from the session
     $studentNumber = $_SESSION["student_number"];
 
+    //checks if student number already has a ballot
+    $result = "SELECT student_number FROM ballot WHERE student_number = $studentNumber";
+
+    //if no rows = student number
     if(mysqli_num_rows($result) <= 0){
-        $sql = "INSERT INTO ballot (date, student_number) VALUES (NOW(), $studentNumber)";
 
-        $sql = "SELECT ballot_id FROM ballot WHERE student_number = $studentNumber";
+        //create new ballot for student number
+        $createBallot = "INSERT INTO ballot (date, student_number) VALUES (NOW(), $studentNumber)";
 
+        //selects the ballot id that is linked to the student number
+        $selectBallot = "SELECT ballot_id FROM ballot WHERE student_number = $studentNumber";
+
+        //records each vote
         foreach($_POST as $vote){
+
+            //inserts candidate id, ballot id and date in vote table
             $candidate = $vote['candidate_id'];
             $sql = "INSERT INTO vote (candidate_id, ballot_id, date) VALUES ($candidate, ballot_id, NOW())"
         }
